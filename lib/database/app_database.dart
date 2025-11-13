1486930a6c3ece8f97b01b644ea51434843adc1f
Narak Sarat <saratnarak85@gmail.com>
2025-11-13 12:21:54 +0700

    Update lib/ from upstream repository

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
+import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:taskapp/data/model/category_model.dart';
 
 class AppDatabase {
-  AppDatabase._(); //is a private constructor
+  AppDatabase._();
   static final AppDatabase instance = AppDatabase._();
   static Database? _database;
 
@@ -12,26 +13,33 @@ class AppDatabase {
 
   Future<Database> _init() async {
     Directory directory = await getApplicationDocumentsDirectory();
-    String path = "${directory.path}task_app.db";
+    // Use path.join to ensure a correct path separator
+    String path = p.join(directory.path, 'task_app.db');
 
     return await openDatabase(
       path,
       version: 1,
-      onOpen: (db) {},
-      onCreate: (db, version) async{
+      onOpen: (db) {
+        // helpful for debugging
+        print('Database opened at: $path');
+      },
+      onCreate: (db, version) async {
         await _onCreateTable(db.batch());
       },
-      onUpgrade: (db, oldVersion , newVersion){}
+      onUpgrade: (db, oldVersion, newVersion) {},
     );
   }
 
   Future<void> _onCreateTable(Batch batch) async {
     batch.execute(
-      '''' CREATE TABLE ${CategoryModel.tableName}(
-          id INTEGER PRIMARY KEY AUTOINCREMENT, 
-          title TEXT , 
-          dateTime TEXT)'''
+      // correct triple-quoted SQL string, no extra quote
+      '''CREATE TABLE ${CategoryModel.tableName}(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          dateTime TEXT
+        )''',
     );
     await batch.commit();
   }
 }
+
